f1b21ac1ef0b031449c59080ab08eabef5311c51
andrewwwmatsko <matskoandrew@gmail.com>
2024-08-09 20:46:24 +0300

    project set up for typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.jsx";
+import App from "./components/App/App.js";
 import "./index.css";
 import Modal from "react-modal";
 
 Modal.setAppElement("#root");
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
